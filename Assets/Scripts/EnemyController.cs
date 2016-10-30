2e14bc1e6b81a41422789bc2b8afadde88b03a2d
Umit Karasu <umitkarasu@yahoo.ca>
2016-10-30 09:32:07 -0400

    hit sound added for collisions

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,6 +10,13 @@ public class EnemyController : MonoBehaviour {
 	//create rigid body component for enemies  done
 	private Rigidbody2D rigidbodyComponent;
 
+	//audioclip which plays when objects collieded
+	public AudioClip hitsound;
+
+	//source for audioclip            1
+	AudioSource audioEnemy;
+
+
 	// Use this for initialization
 	void Start () {
 		// Get the rigidbody component  done
@@ -17,6 +24,8 @@ public class EnemyController : MonoBehaviour {
 
 		// Add a horizantol speed to the enemy
 		rigidbodyComponent.velocity = new Vector2(-speed,0);
+
+
 	}
 
 	// method called when the enemies,obstacles or coins(with crazy_bullet) collides with the fireball and the crazy_bullet
@@ -27,20 +36,30 @@ public class EnemyController : MonoBehaviour {
 		//get the name/s of enemies,obstacles or coin
 		string enemyName = gameObject.name;
 
+		//get audio source component of the connected objects(enemy,obstacle or coin)
+		audioEnemy = gameObject.GetComponent<AudioSource> ();
+
 		// If the enemy collided with the fireball
 		if (name == "fireball(Clone)") {
 
-				//Destroy fireball
-				Destroy (obj.gameObject);
-				//destroy enemy(birds)
-				Destroy (gameObject);
+			//play hit sound which is connected to the bird  1
+			audioEnemy.PlayOneShot (hitsound);
+			//delay object destroy for a while to complete of the playing sound 1
+			//Destroy fireball done
+			Object.Destroy (obj.gameObject,0.15f);
+			//destroy enemy(birds) done
+			Object.Destroy (gameObject,0.15f);
 
 			}
 
 		// If the enemy,obstcale or coin collided with the carzy_bullet
 		if (name == "crazy_bullet") {
 
-			Destroy (gameObject);
+			//if crazy_bullet collides with birds
+			//play hit sound for birds
+			audioEnemy.PlayOneShot (hitsound);
+			//delay object destroy for a while to complete of the playing sound
+			Object.Destroy (gameObject,0.15f);
 
 		}
 
