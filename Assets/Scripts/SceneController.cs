418c447057d6d818acafa9345abcbc27af3378e2
Umit Karasu <umitkarasu@yahoo.ca>
2016-10-30 14:58:23 -0400

    SceneController script added

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController: MonoBehaviour {
+
+	//load the  desired scene
+	public void loadScene(string scene){
+
+		SceneManager.LoadScene (scene);
+	}
+	//quit application does not work  at in development mode, but works after "build and run"
+	public void quit(){
+		Application.Quit ();
+	}
+}
