541a066b0e023b37a0c23fd40717092ede1aafe9
Umit Karasu <umitkarasu@yahoo.ca>
2016-10-30 19:08:23 -0400

    variable name changed

--- Assets/Scripts/RandomEnemyController.cs
+++ Assets/Scripts/RandomEnemyController.cs
@@ -6,7 +6,7 @@ public class RandomEnemyController : MonoBehaviour {
 	public GameObject[] enemy;
 	private Transform _transform;
 	private Vector2 spawnPoint;
-	private SpriteRenderer renderer;
+	private SpriteRenderer rend;
 	// Variable to know how fast we should create new enemies or obstc
 	public float spawnTime ;
 
@@ -19,15 +19,15 @@ public class RandomEnemyController : MonoBehaviour {
 	// method to spawn an enemy
 	void  addEnemy (){
 		// Get the renderer component of the spawn object
-		if(renderer==null)
-			renderer =gameObject.GetComponent<SpriteRenderer>();
+		if(rend==null)
+			rend =gameObject.GetComponent<SpriteRenderer>();
 
 		// Position of the left edge of the spawn object
 		// position of the center-half the width
-		float y1= transform.position.y - renderer.bounds.size.y/2;
+		float y1= transform.position.y - rend.bounds.size.y/2;
 
 		// Same for the right edge
-		float y2= transform.position.y + renderer.bounds.size.y/2;
+		float y2= transform.position.y + rend.bounds.size.y/2;
 
 		// Randomly pick a point within the spawn object
 		spawnPoint= new Vector2(transform.position.x,Random.Range(y1, y2));
